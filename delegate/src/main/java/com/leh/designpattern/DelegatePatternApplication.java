67935dd83d0832e2d71278bda060f0002cf4e5e3
lveh <lveh@tsfa.com.cn>
2019-06-19 15:39:25 +0800

    design demo

---
+++ delegate/src/main/java/com/leh/designpattern/DelegatePatternApplication.java
@@ -0,0 +1,13 @@
+package com.leh.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelegatePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelegatePatternApplication.class, args);
+	}
+
+}
---
+++ delegate/src/main/java/com/leh/designpattern/delegate/IDelegate.java
@@ -0,0 +1,9 @@
+package com.leh.designpattern.delegate;
+
+/**
+ * @Auther: leh
+ * @Date: 2019/6/19 15:32
+ * @Description:
+ */
+public interface IDelegate {
+}
